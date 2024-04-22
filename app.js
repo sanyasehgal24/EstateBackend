d6b31df8f9aa0f1fbc1972956f09ff71684e7995
Sanya Sehgal <sanyasehgal2405@gmail.com>
2024-04-23 04:47:13 +0530

    changes done

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ import chatRoute from "./routes/chat.route.js";
 import messageRoute from "./routes/message.route.js";
 
 const app = express();
-app.use(cors({
-  origin: "http://localhost:5173",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-}));
-// app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+// app.use(cors({
+//   origin: "http://localhost:5173",
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+//   credentials: true
+// }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
